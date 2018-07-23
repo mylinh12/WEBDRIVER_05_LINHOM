2ba0185332baa17baf7f90180c86962a82fbc21e
MyLinh <shinichi121088@gmail.com>
2018-07-23 21:20:23 +0700

    Topic_03: Add TC_02

--- src/selenium_api/Topic_03_WebElement_WebBrowser.java
+++ src/selenium_api/Topic_03_WebElement_WebBrowser.java
@@ -68,7 +68,102 @@ public class Topic_03_WebElement_WebBrowser {
 	
 	@Test
 	public void TC_02() {
+		System.out.println("Check <Email/ Age (Under 18)/ Education/ Job Role 01/ Interests (Development)/ Slider 01/> Button is enabled");
 		
+		// Define variables
+		WebElement email, ageUnder18, education, jobRole01, interestsDevelopment, slider01, 
+		password,ageRadiobuttonIsDisabled, riography, jobRole02, interestsCheckboxIsDisabled, slider02;
+
+		// Locator of elements
+		email = driver.findElement(By.xpath("//input[@id='mail']"));		
+		ageUnder18 = driver.findElement(By.xpath("//input[@id='under_18']"));
+		education = driver.findElement(By.xpath("//textarea[@id='edu']"));
+		jobRole01 = driver.findElement(By.xpath("//select[@id='job1']"));
+		interestsDevelopment = driver.findElement(By.xpath("//input[@id='development']"));
+		slider01 = driver.findElement(By.xpath("//input[@id='slider-1']"));
+		password = driver.findElement(By.xpath("//input[@id='password']"));
+		ageRadiobuttonIsDisabled = driver.findElement(By.xpath("//input[@id='radio-disabled']"));
+		riography = driver.findElement(By.xpath("//textarea[@id='bio']"));
+		jobRole02 =driver.findElement(By.xpath("//select[@id='job2']"));
+		interestsCheckboxIsDisabled = driver.findElement(By.xpath("//input[@id='check-disbaled']"));
+		slider02 = driver.findElement(By.xpath("//input[@id='slider-2']"));
+		
+		// Check elements are enabled
+		System.out.println("Check elements are enabled, if that element is enabled, print message '<that element> is enabled'");
+		if(email.isEnabled()) {
+			System.out.println("<Email> field is enabled");
+		} else {
+			System.out.println("<Email> field is disable");
+		}
+		
+		if(ageUnder18.isEnabled()) {
+			System.out.println("<Age (Under 18)> field is enabled");
+		} else {
+			System.out.println("<Age (Under 18)> field is disable");
+		}
+		
+		if(education.isEnabled()) {
+			System.out.println("<Education> field is enabled");
+		} else {
+			System.out.println("<Education> field is disable");
+		}
+		
+		if(jobRole01.isEnabled()) {
+			System.out.println("<Job Role 01> field is enabled");
+		} else {
+			System.out.println("<Job Role 01> field is disable");
+		}
+		
+		if(interestsDevelopment.isEnabled()) {
+			System.out.println("<Interests - Development> field is enabled");
+		} else {
+			System.out.println("<Interests - Development> field is disable");
+		}
+		
+		if(slider01.isEnabled()) {
+			System.out.println("<Slider 01> field is enabled");
+		} else {
+			System.out.println("Slider> 01 field is disable");
+		}
+		
+		// Check elements are disabled
+		System.out.println("--------------- Check elements are disabled ---------------");
+		
+		if(password.isEnabled()==false) {
+			System.out.println("<Password> field is disable");
+		} else {
+			System.out.println("<Password> field is enabled");
+		}
+		
+		if(ageRadiobuttonIsDisabled.isEnabled() == false) {
+			System.out.println("<Age Radiobutton Is Disabled> field is disable");
+		} else {
+			System.out.println("<Age Radiobutton Is Disabled> field is enabled");
+		}
+		
+		if(riography.isEnabled()== false) {
+			System.out.println("<Riography> field is disable");
+		} else {
+			System.out.println("<Riography> field is enabled");
+		}
+		
+		if(jobRole02.isEnabled()) {
+			System.out.println("<Job Role 02> field is disable");
+		} else {
+			System.out.println("<Job Role 02> field is enabled");
+		}
+		
+		if(interestsCheckboxIsDisabled.isEnabled()) {
+			System.out.println("<Interests - Checkbox is disabled> field is disable");
+		} else {
+			System.out.println("<Interests - Checkbox is disabled> field is enabled");
+		}
+		
+		if(slider02.isEnabled()) {
+			System.out.println("<Slider 02> field is disable");
+		} else {
+			System.out.println("<Slider 02> field is enabled");
+		}
 	}
 
 	@AfterClass
